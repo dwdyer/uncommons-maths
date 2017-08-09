05f5ca3f4fc07c17986decf5e5edb3ee53c59e45
Chris Hennick <seahen123@gmail.com>
2017-08-09 01:31:56 +0000

    Revert "Allows uncommons-math tests to run on Travis-CI" to isolate this branch from the Travis branch.
    This reverts commit 87f6586ced42b816f315682a59bfcd4dee178673.

--- lib/compiletime/cobertura/cobertura-check.sh
+++
@@ -1,2 +0,0 @@
-BASEDIR=`dirname $0`
-java -cp $BASEDIR/cobertura-2.1.1.jar:$BASEDIR/lib/asm-5.0.1.jar:$BASEDIR/lib/asm-tree-5.0.1.jar:$BASEDIR/lib/asm-commons-5.0.1.jar:$BASEDIR/lib/asm-util-5.0.1.jar:$BASEDIR/lib/commons-lang3-3.3.2.jar:$BASEDIR/lib/slf4j-api-1.7.5.jar:$BASEDIR/lib/logback-core-1.0.13.jar:$BASEDIR/lib/logback-classic-1.0.13.jar:$BASEDIR/lib/oro-2.0.8.jar net.sourceforge.cobertura.check.CheckCoverageMain $*
